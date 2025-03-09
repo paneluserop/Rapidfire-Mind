9baa72083c6ee30251f10c57be6d32061d0a705e
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:07:08 +0530

    Update commandicons.js

--- UI/icons/commandicons.js
+++ UI/icons/commandicons.js
@@ -8,7 +8,7 @@ const cmdIcons = {
     msgIcon : 'https://cdn.discordapp.com/emojis/915167284648116244.png',
     rippleIcon : 'https://cdn.discordapp.com/emojis/903301195307843595.gif',
     levelUpIcon : 'https://cdn.discordapp.com/emojis/908223980438163476.gif',
-    SSRRIcon : 'https://cdn.discordapp.com/emojis/1334648756649590805.png'
+    SSRRIcon : 'https://cdn.discordapp.com/attachments/1300829409292587078/1348213956199583765/IMG_20250309_140839_880.jpg?ex=67cea597&is=67cd5417&hm=3bd3363526baf63326b6138e5b140c66579fe71706adfc8238011b0298891e85&'
 };
 
 module.exports = cmdIcons;
