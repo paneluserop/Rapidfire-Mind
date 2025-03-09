ca745f10b3d7f3e8edda17df25109a035dffc171
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:57:15 +0530

    Update seek.js

--- commands/distube/seek.js
+++ commands/distube/seek.js
@@ -78,7 +78,7 @@ module.exports = {
                     .setAuthor({ 
                         name: lang.seekNoQueueTitle, 
                         iconURL: musicIcons.wrongIcon,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/8JkTHpsz4k"
                     })
                     .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })
                     .setDescription(lang.seekNoQueueMessage);
