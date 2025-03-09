d22b78a2c397ba87cb60ab750c84b6d336564feb
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:09:25 +0530

    Update afk.js

--- commands/utility/afk.js
+++ commands/utility/afk.js
@@ -145,7 +145,7 @@ module.exports = {
                 .setAuthor({
                     name: "Alert!",
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/rapidfirecorp"
                 })
                 .setDescription('- This command can only be used through slash command!\n- Please use `/afk`')
                 .setTimestamp();
