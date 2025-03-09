d9bc858e1accd3c0cfac9735154512c66b8cb6ac
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:04:27 +0530

    Update twitchHandler.js

--- events/twitchHandler.js
+++ events/twitchHandler.js
@@ -35,7 +35,7 @@ async function fetchTwitchStreams(client) {
                     .setAuthor({
                         name: 'New content Uploaded!',
                         iconURL: cmdIcons.TwitchIcon,
-                        url: 'https://discord.gg/xQF9f9yUEM', 
+                        url: 'https://discord.gg/rapidfirecorp', 
                     })
                     .setDescription(`[Watch Now](https://www.twitch.tv/${latestStream.user_name})`)
                     .addFields({ name: 'Game', value: latestStream.game_name, inline: true })
