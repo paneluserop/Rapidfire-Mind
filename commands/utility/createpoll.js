9c4b43422c62458b499609c804ad10022926ced0
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:10:09 +0530

    Update createpoll.js

--- commands/utility/createpoll.js
+++ commands/utility/createpoll.js
@@ -110,7 +110,7 @@ module.exports = {
                 .setAuthor({
                     name: "Alert!",
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/rapidfirecorp"
                 })
                 .setDescription(lang.pollCommandOnly)
                 .setTimestamp();
