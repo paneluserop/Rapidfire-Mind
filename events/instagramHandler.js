abe77eecaceaba060dede52734329231d40cf598
paneluserop <abhijeetpanda946@gmail.com>
2025-03-09 18:00:05 +0530

    Update instagramHandler.js

--- events/instagramHandler.js
+++ events/instagramHandler.js
@@ -31,7 +31,7 @@ async function fetchInstagramPosts(client) {
                     .setAuthor({
                         name: 'New Instagram Post!',
                         iconURL: cmdIcons.InstagramIcon,
-                        url: 'https://discord.gg/xQF9f9yUEM', 
+                        url: 'https://discord.gg/rapidfirecorp', 
                     })
                     .setDescription(latestMedia.caption || 'No caption')
                     .setImage(latestMedia.media_url)
